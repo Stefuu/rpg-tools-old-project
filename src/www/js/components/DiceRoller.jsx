4a130c5f2072c25590b4b975dcb9d04b0972b947
André Duarte <eu@andreduartesp.net>
2015-07-13 14:57:10 -0300

    Ajuste para ser usada o dado selecionado

--- src/www/js/components/DiceRoller.jsx
+++ src/www/js/components/DiceRoller.jsx
@@ -8,7 +8,14 @@ var DiceRoller = React.createClass({
     this.setState({dicePositions : 10});
   },
   _rollDice: function(){
-    this.setState({diceResult : Math.round(Math.random() * (this.state.dicePositions - 1) + 1)});
+    this.setState({diceResult : Math.round(Math.random() * (document.querySelector('#dicesList li.dicePositionsSelected').id - 1) + 1)});
+  },
+  _selectDicePositions: function(event){
+    var elements = document.querySelectorAll('#dicesList li');
+    Array.prototype.forEach.call(elements, function(el, i){
+      el.classList.remove('dicePositionsSelected');
+    });
+    event.currentTarget.classList.add('dicePositionsSelected');
   },
   render: function() {
     return (
@@ -20,12 +27,24 @@ var DiceRoller = React.createClass({
         </div>
         <div className="listDiceContainer">
           <ul id="dicesList">
-            <li><img src="assets/img/icon-d4.png" /></li>
-            <li><img src="assets/img/icon-d6.png" /></li>
-            <li><img src="assets/img/icon-d8.png" /></li>
-            <li><img src="assets/img/icon-d10.png" /></li>
-            <li><img src="assets/img/icon-d12.png" /></li>
-            <li><img src="assets/img/icon-d20.png" /></li>
+            <li id="4" onClick={this._selectDicePositions}>
+              <img src="assets/img/icon-d4.png" />
+            </li>
+            <li id="6" onClick={this._selectDicePositions}>
+              <img src="assets/img/icon-d6.png" />
+            </li>
+            <li id="8" onClick={this._selectDicePositions}>
+              <img src="assets/img/icon-d8.png" />
+            </li>
+            <li id="10" onClick={this._selectDicePositions} className="dicePositionsSelected">
+              <img src="assets/img/icon-d10.png" />
+            </li>
+            <li id="12" onClick={this._selectDicePositions}>
+              <img src="assets/img/icon-d12.png" />
+            </li>
+            <li id="20" onClick={this._selectDicePositions}>
+              <img src="assets/img/icon-d20.png" />
+            </li>
           </ul>
         </div>
       </div>
