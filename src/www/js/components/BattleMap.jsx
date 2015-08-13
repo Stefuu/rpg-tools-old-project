156f127481705b4efa84a76907ba0101e6c2efb0
Stefano Damiano <sdamiano@uolinc.com>
2015-08-13 18:46:05 -0300

    Ordenando friend/enemies

--- src/www/js/components/BattleMap.jsx
+++ src/www/js/components/BattleMap.jsx
@@ -7,17 +7,19 @@ var Char = require('./Char.jsx');
 
 var BattleMap = React.createClass({
 	getInitialState: function() {
-    	return {qtdChars: 0};
+    	return {
+    		totChars: 0
+    	};
   	},
-	_createMatrix: function(size){
+	_createMatrix: function(sizeX,sizeY){
 		var matrix = []; 
 		var	i;
 		var j;
 
-		for( i = 0; i < size; i++ ){
+		for( i = 0; i < sizeX; i++ ){
 			matrix.push([]);
 
-			for( j = 0; j < size; j++ ){
+			for( j = 0; j < sizeY; j++ ){
 				matrix[i].push( i.toString() + j);				
 			}
 		}
@@ -25,14 +27,12 @@ var BattleMap = React.createClass({
 		return matrix;
 	},
 
-	_tiles: function(size){
+	_tiles: function(sizeX,sizeY){
 
-		var size = size || 10;
-
-		var matrix = this._createMatrix(size);
+		var matrix = this._createMatrix(sizeX,sizeY);
 
 		var markup = [];
-		for(var i = 0; i < size; i++){
+		for(var i = 0; i < sizeX; i++){
       		markup.push(<DynamicRow itens={matrix[i]}/>)
     	}
 
@@ -44,24 +44,36 @@ var BattleMap = React.createClass({
 		
 		var chars = [];
 		
-		for( i = 0; i < this.state.qtdChars; i++ ){
-			chars.push(<Char />);
+		for( i = 0; i < this.state.totChars; i++ ){
+			if( this._charOrder[i] == 'friend' ){	
+				chars.push(<Char type={'friend'} key={i} num={i} />);
+			}else{
+				chars.push(<Char type={'enemy'} key={i} num={i} />);
+			}
 		}
+		
 
 		return chars;
 	},
+	
+	_charOrder: [],
 
 	_addChar: function(type){
-		this.setState({qtdChars: this.state.qtdChars + 1});
+		this._charOrder.push(type);
+		this.setState({
+			totChars: this.state.totChars + 1
+		});
 	},
 
 	_clear: function(type){
-		this.setState({qtdChars: 0});
+		this.setState({
+			totChars: 0
+		});
 	},
 
 	render: function() {
     
-	var matrix = this._tiles(10);
+	var matrix = this._tiles(12,7);
 	var chars = this._createChars();
 
     return (
@@ -74,13 +86,13 @@ var BattleMap = React.createClass({
         </table>
         <div className="controls">
         	<div className="friend" onClick={function(){ this._addChar('friend'); }.bind(this)}>
-        		friend
+        		Add Friend
         	</div>
         	<div className="enemy" onClick={function(){ this._addChar('enemy'); }.bind(this)}>
-        		enemy
+        		Add Enemy
         	</div>
         	<div className="clear" onClick={this._clear}>
-        		clear
+        		Delete All
         	</div>
         </div>
       </div>
