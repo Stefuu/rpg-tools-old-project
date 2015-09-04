8e08d0813b038b22179d43a2378961b1496b86a7
André Duarte <aguimaraes@uolinc.com.br>
2015-09-03 21:33:32 -0300

    Ajustada mensagem de sem dados

--- src/www/js/components/DiceRoller.jsx
+++ src/www/js/components/DiceRoller.jsx
@@ -49,9 +49,6 @@ var DiceRoller = React.createClass({
           </li>);
       }
     }
-    else{
-      renderedDiceList.push(<span>Selecione pelo menos um dado</span>);
-    }
     return (
       <div className="dice-roller">
         <div className="rollDiceContainer">
@@ -60,6 +57,7 @@ var DiceRoller = React.createClass({
             <ul id="selectedDicesList">
               {renderedDiceList}
             </ul>
+            {this.state.dices.length <= 0 ? <span className="no-selected-dice">Selecione pelo manos um dado</span> : false}
           </div>
           <div className="rollDiceResult">
             <div className="label">
