672f0b2ed6bcd415d129fab5fe78a2249994bd7c
Stefano Damiano <sdamiano@uolinc.com>
2015-10-07 13:16:14 -0300

    Replicando rotas em pt-br

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -21,7 +21,7 @@ gulp.task('css', function() {
             browsers: ['> 5%'],
             cascade: false
         }))
-    .pipe(concat('bundle.css'))
+    .pipe(concat('main.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('./src/www/dist/css/'));
 
